1673ddb877b0ccb59358fed4717b60ea5be5feba
sreekanth avuleti <sreekanthavuleti@gmail.com>
2025-05-29 14:44:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>i am a nani js developer</h1>
+      <h4>hi</h4>
+    </div>
+  );
+}
+
+export default App;
